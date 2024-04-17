1461ec29e3b129e307b499a62297f754e0704e29
Viviane <dessaintviviane@gmail.com>
2024-04-17 04:08:23 +0200

    Update page.js with dark mode support, new layout, social media icons, and service offerings

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 'use client'
 
 import {BsFillMoonStarsFill} from 'react-icons/bs';
-import {AiFillLinkedin, AiFillYoutube, AiFillTwitterCircle } from 'react-icons/ai';
+import {AiFillLinkedin, AiFillGithub } from 'react-icons/ai';
 import Image from "next/image";
 import profile from "../public/profile.png";
 import design from '../public/design.png';
@@ -17,33 +17,39 @@ export default function Home() {
 
   return (
     <div className={darkMode ? 'dark' : ""}>
+      <div className='sticky top-0'>
+        <nav className='px-10 md:px-20 lg:px-40 py-10 mb-12 flex justify-between bg-white dark:bg-gray-900 dark:text-white'>
+          <h1 className='text-xl font-JetBrains_MonoBold'>VIVIANE&apos;S PORTFOLIO</h1>
+          <ul className='flex items-center ml-4'>
+            <li>
+              <BsFillMoonStarsFill onClick={() => setDarkMode(!darkMode)} className='cursor-pointer text-2xl'/>
+            </li>
+            <li>
+              <a className='font-JetBrains_MonoBold bg-gradient-to-r from-violet-300 to-purple-500 text-white px-4 py-2 rounded-md ml-8' href="/CV.pdf" download="Viviane Dessaint - CV" >Resume</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+
       <main className='bg-white px-10 md:px-20 lg:px-40 dark:bg-gray-900'>
-        <section className="min-h-screen">
-          <nav className='sticky top-0 py-10 mb-12 flex justify-between bg-white dark:bg-gray-900 dark:text-white'>
-            <h1 className='text-xl font-JetBrains_MonoBold'>VIVIANE&apos;S PORTFOLIO</h1>
-            <ul className='flex items-center'>
-              <li>
-                <BsFillMoonStarsFill onClick={() => setDarkMode(!darkMode)} className='cursor-pointer text-2xl'/>
-              </li>
-              <li>
-                <a className='font-JetBrains_MonoBold bg-gradient-to-r from-violet-300 to-purple-500 text-white px-4 py-2 rounded-md ml-8' href="/CV.pdf" download="Viviane Dessaint - CV" >Resume</a>
-              </li>
-            </ul>
-          </nav>
+        <section className="min-h-screen">          
           <div className='text-center p-10'>
             <h2 className='text-5xl py-2 text-purple-500 font-JetBrains_Mono font-medium md:text-6xl dark:text-violet-300'>
               Viviane Dessaint
             </h2>
             <h3 className='text-2xl py-2 md:text-3xl dark:text-white'>
-              Web developper
+              Web and mobile developer
             </h3>
             <p className='text-md py-5 leading-8 text-gray-800 md:text-xl max-w-lg mx-auto dark:text-gray-200'>
-              Join me
+              Developing SASS mobile and web applications with React Native, ReactJS and NextJS.
             </p>
             <div className='text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400'>
-              <AiFillTwitterCircle />
-              <AiFillLinkedin />
-              <AiFillYoutube />
+              <a href='https://www.linkedin.com/in/vivianedessaint/'>
+                <AiFillLinkedin />
+              </a>
+              <a href="https://github.com/Jubstacheit">
+                <AiFillGithub />
+              </a>
             </div>
             <div className='p-2 mx-auto bg-gradient-to-b from-blue-400 rounded-full w-60 h-60 mt-20 overflow-hidden md:h-96 md:w-96'>
               <Image alt='Profile picture' src={profile} objectFit='cover' />
