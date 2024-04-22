572ffd0b811a3ecd25d17a762749804c2f087da5
Viviane <dessaintviviane@gmail.com>
2024-04-22 04:35:29 +0200

    Refactor language toggle component and update toggleIcon styles

--- app/components/toggleIcon.jsx
+++ app/components/toggleIcon.jsx
@@ -12,13 +12,13 @@ export function ToggleIcon({ value, onChange, options }) {
   return (
     <div className="relative inline-block text-left">
       <div>
-        <button type="button" className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500" id="options-menu" aria-haspopup="true" aria-expanded="true" onClick={() => setIsOpen(!isOpen)}>
+        <button type="button" className="inline-flex justify-center w-full rounded-lg p-2 text-purple-500 dark:text-violet-300 hover:bg-gray-100 dark:hover:bg-gray-950" id="options-menu" aria-haspopup="true" aria-expanded="true" onClick={() => setIsOpen(!isOpen)}>
           <TbLanguage className='text-2xl cursor-pointer'/>
         </button>
       </div>
 
       {isOpen && (
-        <div className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
+        <div className="origin-top-right absolute right-0 mt-2 w-12 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
           <div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
             {options.map((option) => (
               <a key={option.value} className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem" onClick={() => handleClick(option.value)}>
