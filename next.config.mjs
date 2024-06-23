03df45c9048956d296faa3787fa75a3b6246e196
Viviane <dessaintviviane@gmail.com>
2024-06-24 01:27:42 +0200

    chore: Update .gitignore and package.json

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,15 @@
+import MillionLint from '@million/lint';
 import createNextIntlPlugin from 'next-intl/plugin';
-
 const withNextIntl = createNextIntlPlugin('./i18n.js');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	images: {
-		remotePatterns: [
-			{
-				hostname: 'img.icons8.com',
-			}
-		]
-	},
+  images: {
+    remotePatterns: [{
+      hostname: 'img.icons8.com'
+    }]
+  }
 };
-
-export default withNextIntl(nextConfig);
+export default MillionLint.next({
+  rsc: true
+})(withNextIntl(nextConfig));
